5ba5cfa5ccfcd24dfd4f58f607f1c98fc118818f
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-09-02 22:03:30 +0100

    update ai response

--- src/app/comp/Chat.tsx
+++ src/app/comp/Chat.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { IoCopyOutline } from "react-icons/io5";
+import { SlLike } from "react-icons/sl";
 function Chat() {
   return (
     <div className="h-full w-full flex items-start overflow-y-auto flex-col gap-8 p-4">
@@ -9,8 +10,19 @@ function Chat() {
       </div>
 
       {/* AI response */}
-      <div className=" text-gray-200 p-3 rounded-xl self-start">
-        Sure! What do you need help with?
+      <div className=" group p-3 rounded-xl self-start">
+        <section className="flex gap-2 items-center justify-center">
+          <div className="h-[30px] w-[30px] border border-gray-300 rounded-full "></div>
+          <p>Sure! What do you need help with?</p>
+        </section>
+        <section className="w-[80px] text-gray-200 p-2 items-center justify-start translate-x-[45px] rounded-[10px] border hidden group-hover:flex border-zinc-700 h-[37px] ">
+          <div className="p-2 rounded-md hover:bg-zinc-700 transition-all flex-center  ">
+            <IoCopyOutline size={15} />
+          </div>
+          <div className="p-2 rounded-md hover:bg-zinc-700 transition-all flex-center  ">
+            <SlLike size={15} />
+          </div>
+        </section>
       </div>
     </div>
   );
