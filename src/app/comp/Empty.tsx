597c6c194fecab18aa6e03fcf5db409b962d761c
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-09-02 16:45:10 +0100

    added search

--- src/app/comp/Empty.tsx
+++ src/app/comp/Empty.tsx
@@ -1,19 +1,48 @@
 import React from "react";
+import { MdHelp } from "react-icons/md";
+import { FiCheckCircle } from "react-icons/fi";
+import { AiOutlineMessage } from "react-icons/ai";
+import { FaQuestionCircle } from "react-icons/fa";
 import { TbSquareRoundedLetterYFilled } from "react-icons/tb";
 function Empty() {
+  const prompts = [
+    {
+      text: "Kini o le ṣe fun ọ loni?",
+      icon: MdHelp,
+      color: "text-red-400",
+    },
+    {
+      text: "Bawo ni mo ṣe le ran ọ lọwọ?",
+      icon: FiCheckCircle,
+      color: "text-green-400",
+    },
+    {
+      text: "Kini ọrọ ti o fẹran lati jiroro?",
+      icon: AiOutlineMessage,
+      color: "text-yellow-400",
+    },
+    {
+      text: "Ṣe o ni ibeere eyikeyi nipa ọrọ kan?",
+      icon: FaQuestionCircle,
+      color: "text-purple-400",
+    },
+  ];
+
   return (
-    <div className="h-full w-full flex-center flex-col gap-8  ">
+    <div className="h-full w-full flex-center flex-col gap-8">
       <div>
         <TbSquareRoundedLetterYFilled className="text-gray-200" size={70} />
       </div>
       <div className="flex gap-3">
-        {[1, 2, 3, 4].map((d, id) => {
+        {prompts.map((prompt, id) => {
+          const IconComponent = prompt.icon;
           return (
-            <div className="h-[150px] w-40 pt-3 pb-4 flex flex-col px-3 gap-2 border border-zinc-700 rounded-2xl ">
-              <TbSquareRoundedLetterYFilled size={30} />
-              <p className="text-[14px] text-start ">
-                Create an image for my presentation
-              </p>
+            <div
+              key={id}
+              className="h-[150px] w-40 pt-3 pb-4 flex flex-col px-3 gap-2 border border-zinc-700 rounded-2xl"
+            >
+              <IconComponent className={prompt.color} size={20} />
+              <p className="text-[14px] text-start">{prompt.text}</p>
             </div>
           );
         })}
--- src/app/comp/Main.tsx
+++ src/app/comp/Main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { RxUpload } from "react-icons/rx";
 import Empty from "./Empty";
+import { TbSquareRoundedLetterYFilled } from "react-icons/tb";
+import { HiOutlineArrowSmUp } from "react-icons/hi";
 function Main() {
   return (
     <main className="flex flex-col w-full flex-1 h-full items-start ">
@@ -15,9 +17,22 @@ function Main() {
           <div className="flex-center h-[40px] w-[40px] rounded-full bg-orange-500 "></div>
         </div>
       </nav>
-      <main className="flex w-full h-full  items-center justify-center">
+      <main className="flex w-full h-full flex-1  items-center justify-center">
         {true ? <Empty /> : <div></div>}
       </main>
+      <footer className="flex-center h-[100px]  w-full   ">
+        <div className="w-[80%] bg-neutral-700 px-2 flex-center h-[50px] rounded-full ">
+          <input
+            type="text"
+            className="bg-neutral-700 px-4 text-white border-none flex-1  outline-none  "
+            name=""
+            id=""
+          />
+          <button className="h-[30px] w-[30px] bg-gray-200 rounded-full   ">
+            <HiOutlineArrowSmUp size={30} />
+          </button>
+        </div>
+      </footer>
     </main>
   );
 }
