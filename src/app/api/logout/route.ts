793667bb01ce5e4e6adf238b51b5dcbb553526e8
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-09-12 09:15:02 +0100

    resolving vercel bug

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,12 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(resp: NextResponse) {
+export async function GET(req: NextRequest) {
   try {
     const response = NextResponse.json({
-      message: "Logout successfull",
-      success: "true",
+      message: "Logout successful",
+      success: true,
     });
-    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+
+    // Set the cookie for logout
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0), // Expire immediately to log out
+    });
+
     return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
